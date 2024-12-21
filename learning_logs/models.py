e469be3fc0e3b96b7955a2cd8e9040731ac50a42
Napoleao1 <ernaninandoxd@gmail.com>
2024-12-21 11:57:59 -0300

    criando modelo de tabela no bd

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -6,4 +6,6 @@ class Topic(models.Model):
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
     
-  def __str__(self)
+  def __str__(self):
+      """Devolve uma representação em string do modelo"""
+      return self.text
