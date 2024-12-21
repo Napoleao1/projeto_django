fa443dd35123af5134c79f29df79ae4c3bc47c3c
Napoleao1 <ernaninandoxd@gmail.com>
2024-12-21 11:53:48 -0300

    criando metódos

---
+++ learning_logs/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Topic(models.Model):
+    """Um assunto sobre qual o usuário está aprendendo"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+  def __str__(self)
